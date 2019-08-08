1d6418cd5d1640ef7175e8d83962e50ed6991524
Tilmann Meyer <allescrafterx@gmail.com>
2019-08-08 23:42:47 +0200

    Fixxed stuff

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
 var HotkeylessAHKServer = /** @class */ (function () {
     function HotkeylessAHKServer(serverPort) {
         var _this = this;
         this.serverPort = serverPort;
         // Setup server
-        this.app = express();
-        this.router = express.Router();
+        this.app = express_1["default"]();
+        this.router = express_1["default"].Router();
         this.pendingResult = null;
         /**
          * Handles the subscriber aka. redirecting ahk script
@@ -15,11 +18,11 @@ var HotkeylessAHKServer = /** @class */ (function () {
         this.subscriberFunction = function (req, res) {
             if (_this.pendingResult !== null) {
                 console.error("Already subscribed an AHK script with '/subscribe'. Please check your processes!");
-                res.send("");
+                res.send('');
             }
             else {
                 _this.pendingResult = res;
-                console.log("Received subscriber.");
+                console.log('Received subscriber.');
             }
         };
         /**
@@ -30,19 +33,27 @@ var HotkeylessAHKServer = /** @class */ (function () {
                 var command = req.params.command;
                 _this.pendingResult.send(command);
                 _this.pendingResult = null;
-                res.send("success");
+                res.send('success');
                 console.log("Send command: " + command);
             }
             else {
                 console.error("No subscribing process registered. Please call '/subscribe' first!");
-                res.send("failure");
+                res.send('failure');
             }
+        };
+        /**
+         * Stops the node process
+         */
+        this.killFunction = function (req, res) {
+            console.log('Shutting down server...');
+            process.exit(0);
         };
     }
     HotkeylessAHKServer.prototype.setup = function () {
-        console.log("Starting server...");
-        this.router.get("/subscribe", this.subscriberFunction);
-        this.router.get("/send/:command", this.sendFunction);
+        console.log('Starting server...');
+        this.router.get('/subscribe', this.subscriberFunction);
+        this.router.get('/send/:command', this.sendFunction);
+        this.router.get('/kill', this.killFunction);
         // Start server
         this.app.use('/', this.router);
         this.app.listen(this.serverPort);
--- server.ts
+++ server.ts
@@ -1,52 +1,49 @@
 import express, { RequestHandler, Response } from 'express';
 
 export class HotkeylessAHKServer {
+  // Setup server
+  private app = express();
+  private router = express.Router();
 
-    // Setup server
-    private app = express();
-    private router = express.Router();
+  private pendingResult: Response = null;
 
-    private pendingResult: Response = null;
-
-    /**
-     * Handles the subscriber aka. redirecting ahk script
-     */
-    private subscriberFunction: RequestHandler = (req, res) => {
-        if (this.pendingResult !== null) {
-            console.error("Already subscribed an AHK script with '/subscribe'. Please check your processes!");
-            res.send("");
-        } else {
-            this.pendingResult = res;
-            console.log("Received subscriber.");
-        }
-    };
-
-    /**
-     * Handles the sender aka the caller e.g. a stream deck
-     */
-    private sendFunction: RequestHandler = (req, res) => {
-        if (this.pendingResult !== null) {
-            const command = req.params.command;
-            this.pendingResult.send(command);
-            this.pendingResult = null;
-            res.send("success");
-            console.log(`Send command: ${command}`);
-        } else {
-            console.error("No subscribing process registered. Please call '/subscribe' first!");
-            res.send("failure");
-        }
-    };
-
-    /**
-     * Stops the node process
-     */
-    private killFunction: RequestHandler = (req, res) => {
-        console.log("Shutting down server...");
-        process.exit(0);
-    };
-
-    constructor(private serverPort: number) {
+  /**
+   * Handles the subscriber aka. redirecting ahk script
+   */
+  private subscriberFunction: RequestHandler = (req, res) => {
+    if (this.pendingResult !== null) {
+      console.error(
+        "Already subscribed an AHK script with '/subscribe'. Please check your processes!",
+      );
+      res.send('');
+    } else {
+      this.pendingResult = res;
+      console.log('Received subscriber.');
     }
+  };
+
+  /**
+   * Handles the sender aka the caller e.g. a stream deck
+   */
+  private sendFunction: RequestHandler = (req, res) => {
+    if (this.pendingResult !== null) {
+      const command = req.params.command;
+      this.pendingResult.send(command);
+      this.pendingResult = null;
+      res.send('success');
+      console.log(`Send command: ${command}`);
+    } else {
+      console.error("No subscribing process registered. Please call '/subscribe' first!");
+      res.send('failure');
+    }
+  };
+
+  /**
+   * Stops the node process
+   */
+  private killFunction: RequestHandler = (req, res) => {
+    console.log('Shutting down server...');
+    process.exit(0);
   };
 
   constructor(private serverPort: number) {}
@@ -58,10 +55,12 @@ export class HotkeylessAHKServer {
 
     this.router.get('/send/:command', this.sendFunction);
 
+    this.router.get('/kill', this.killFunction);
+
     // Start server
     this.app.use('/', this.router);
     this.app.listen(this.serverPort);
     console.log(`Server running on port ${this.serverPort}.`);
-    console.log("Please use the '/subscribe endpoint first!");
+    console.log("Please use the '/subscribe' endpoint first!");
   }
 }
